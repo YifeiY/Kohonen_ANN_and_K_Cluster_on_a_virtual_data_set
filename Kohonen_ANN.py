086f3e91ae37135960f37c4c5f74138ce7644b40
Yifei <yifei_yin@outlook.com>
2019-03-07 00:08:26 -0500

    made neural net let's call it a day... Good night

--- Kohonen_ANN.py
+++ Kohonen_ANN.py
@@ -1,10 +1,34 @@
 # Yifei Yin 20054101
 
 import csv
+import random
 
+network_config = [3,2]
 def main():
   data = readFile("dataset_noclass.csv")
+  network = make_ANN(network_config)
 
+
+  condition = True
+  while condition:
+    for row in data:
+
+
+
+
+
+
+def feed_data(data, ann):
+  for neuron in ann:
+    neuron['distance'] = 0
+    for i in range(len(neuron['weights'])):
+      neuron['distance'] += neuron['weights'][i] * data[i]
+
+
+
+## Build a 2 layer network with bias
+def make_ANN(config):
+  return [{'weights':[random.random()] * config[0],'distance':0}] * config[1]
 
 
 def readFile(filename):
