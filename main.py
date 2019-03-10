bd4264b6dcaa12b1ded9680b45d0e74882127adc
Yifei <yifei_yin@outlook.com>
2019-03-09 21:55:36 -0500

    Bug in ANN has been fixed: Distance was calculated as the distance from the correct cluster center plus the distance from the incorrect cluster to a particular point. That will generate the same total distance/error at each iteration.

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
 # Yifei Yin 20054101
-
 import random
-
-
 try:
   import matplotlib.pyplot as plt
   from mpl_toolkits.mplot3d import Axes3D
   import numpy as np
 except: pass
-
 
 epsilon = -1/5
 theta = 1
@@ -17,25 +13,24 @@ threshold = 0.1
 output_filename = "output.txt"
 report_filename = "report.txt"
 
+
 def main():
 
   header,data = readFile("dataset_noclass.csv")
   header.append("kohonen")
   header.append("K-Means")
 
-  kohonenANN_out,ANN_center,ANNSSE = kohonenANN(data)
-
-  kMeans_out,KM_center, KMSSE = kMeans(data)
+  kohonenANN_out,ANN_centre,ANNSSE = kohonenANN(data)
+  kMeans_out,KM_centre, KMSSE = kMeans(data)
 
   writeOutput([header] + [data[i] + [kohonenANN_out[i],kMeans_out[i]] for i in range(len(data))])
-  writeReport([ANN_center,ANNSSE],[KM_center,KMSSE])
+  writeReport([ANN_centre,ANNSSE],[KM_centre,KMSSE])
 ## END MAIN
 
 
 ## Use K-Means to identify two clustering
 def kMeans(data):
-
-  # choose two points from the dataset to be the initial center points
+  # choose two points from the dataset to be the initial centre points
   w1,w2 = data[0],data[len(data)//2]
 
   err = 0
@@ -87,21 +82,25 @@ def kMeans(data):
 def kohonenANN(data):
   ann = makeANN(network_config)
 
-  err = 0
-  prev_err = err + 1
+  prev_err = 0
+  err = prev_err + 1
+
   iteration = 0
 
-  while err != prev_err:
+  while err > prev_err:
+    prev_err = err
     err = 0
     iteration += 1
 
     for row in data:
       feedData(row, ann)
       winner = ann[kohonen(ann[0]['distance'], ann[1]['distance'])]
-      err += ann[0]['distance'] + ann[1]['distance']
       updateWeights(row, winner)
 
-    prev_err = err
+    for row in data:
+      feedData(row,ann)
+      winner = ann[kohonen(ann[0]['distance'], ann[1]['distance'])]
+      err += winner['distance']
 
   print("Network converged after", iteration, "iteration.")
 
@@ -109,15 +108,12 @@ def kohonenANN(data):
   errs = [0,0]
   for row in data:
     feedData(row, ann)
-    winner_index = kohonen(ann[0]['distance'], ann[1]['distance']) - 1
-    winner = ann[winner_index]
-    errs[winner_index] += ann[0]['distance'] + ann[1]['distance']
+    winner_index = kohonen(ann[0]['distance'], ann[1]['distance'])
+    errs[winner_index] += ann[winner_index]['distance']
     output.append('A' if winner_index == 0 else 'B')
+
   #showData(data, ann)
-
-
   return output,[node['weights']for node in ann], errs
-
 
 
 ## Feed data to the network, output layer stores distance(d_i,w_i)
@@ -126,7 +122,7 @@ def feedData(row, ann):
     node['distance'] = calculateDistance(node['weights'],row)
 
 
-## use kohonen inhibition to find the winner node
+## use kohonen inhibition (min net) to find the winner node
 def kohonen(a,b):
   if a == b: # when a and b are equal, default a to be the winner
     return 0
@@ -135,7 +131,7 @@ def kohonen(a,b):
     a = max(0, a - 0.5 * b)
     b = max(0, b - 0.5 * old_a)
   if a == 0:
-    return 0
+    return 0 # a is the winner since it was smaller
   else:
     return 1
 
@@ -145,12 +141,11 @@ def updateWeights(data, node):
   rate = eta(node['distance'])
   for i in range(len(node['weights'])):
     node['weights'][i] += rate * (data[i] - node['weights'][i])
-  return sum(node['weights'])
 
 
 ## update the learning rate
 def eta(x):
-  return min(1,x/10)
+  return min(0.9,x/10)
 
 
 ## Calculates the square of Euclidean distance of two vectors
@@ -181,10 +176,10 @@ def showData(data, ann):
   for xs, ys, zs in plot_data[1]:
     ax.scatter(xs, ys, zs, c='b', marker='o', s=0.5)
 
-  center0 = ann[0]['weights']
-  ax.scatter(center0[0], center0[1], center0[2], c='r', marker='*', s=100)
-  center1 = ann[1]['weights']
-  ax.scatter(center1[0], center1[1], center1[2], c='b', marker='*', s=100)
+  centre0 = ann[0]['weights']
+  ax.scatter(centre0[0], centre0[1], centre0[2], c='r', marker='*', s=100)
+  centre1 = ann[1]['weights']
+  ax.scatter(centre1[0], centre1[1], centre1[2], c='b', marker='*', s=100)
   plt.show()
 
 
@@ -192,14 +187,15 @@ def writeReport(ANN,KM,filename = report_filename):
   file = open(filename, "w")
   file.write("Kohonen ANN:"
               "\n\t*3 input nodes:"
-                  "\n\t\t- inputs has 3 arguments"
+                  "\n\t\t- Inputs has 3 arguments"
               "\n\t*2 output nodes:"
-                  "\n\t\t- 2 expected classes"
-              "\n\t*terminates when SSE is the same:"
-                  "\n\t\t- network converges"
-              "\n\t*no iteration limit:"
-                  "\n\t\t- network will always converge"
-              "\n\t*centers:"+
+                  "\n\t\t- There are 2 expected classes"
+              "\n\t*Terminates when SSE remains the same for two iterations:"
+                  "\n\t\t- Network converges"
+              "\n\t*No iteration limit:"
+                  "\n\t\t- This network will always converge using my algorithm"
+                  "\n\t\t- This can be easily proven, but the proof is beyond this assignment."
+              "\n\t*centres:"+
                   "\n\t\t" + str(ANN[0][0]) +
                   "\n\t\t" + str(ANN[0][1]) +
               "\n\t*SSE:"+
@@ -207,18 +203,27 @@ def writeReport(ANN,KM,filename = report_filename):
                   "\n\t\t" + str(ANN[1][1]) +
 
               "\n\nK-Means:"
-              "\n\t*terminates when SSE is the same:"
-                  "\n\t\t- best center point found"
-              "\n\t*centers:" +
+              "\n\t*Terminates when SSE remains the same for two iterations:"
+                  "\n\t\t- Best centre point found"
+              "\n\t*centres:" +
                   "\n\t\t" + str(KM[0][0]) +
                   "\n\t\t" + str(KM[0][1]) +
-              "\n\t*centers:" +
+              "\n\t*SSE:" +
                   "\n\t\t" + str(KM[1][0]) +
-                  "\n\t\t" + str(KM[1][1]))
+                  "\n\t\t" + str(KM[1][1]) +
+              "\n\nAnalysis:"+
+              "\n\t* After plotting data, the centre point of clusters generated from ANN is placed at a visually correct place."
+              "\n\t* The centre point of clusters calculated using K-Means is very close to what sklearn.cluster.KMeans model produced "
+                "and the SSE are almost the same (< 0.01% difference)."
+              "\n\t* So it is safe to say that my K-Means algorithm generates convincing result."
+              "\n\t* The SEE generated by the ANN is larger than that by K-Means (usually +5%)."
+              "\n\t* The reason here is ANN adjust the centre point based on a leaning rate."
+              "\n\t* The adjustment of the centre point will not be efficient if the last input point is very far from the existing centre point "
+                "because the centre point will be dragged to that point by a large amount, ignoring every other points."
+              "\n\t* A way to solve this is to adjust the ANN weights after each epoch and take the average, but that is not consistent with the algorithm discussed in class."
+              "\n\t* Note: It is easy to prove that the ANN algorithm would generate very similar result as K-Means if its weights are adjusted after each epoch.")
 
   print("report has been written to " + "\"" + filename + "\".")
-
-
 
 
 ## write output file
@@ -230,6 +235,7 @@ def writeOutput(content,filename = output_filename):
       line += str(item) + '\t'
     file.write(line + '\n')
   print("output has been written to " + "\"" + filename + "\"." )
+
 
 ## read in file
 def readFile(filename):
